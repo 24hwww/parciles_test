e34e11ccf2107e4d8b6d9ef3f66439852f13024e
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2024-10-22 19:52:52 -0300

    login.php

--- PARCIALES/PARCIAL_3/login.php
+++ PARCIALES/PARCIAL_3/login.php
@@ -7,5 +7,11 @@ $datos_predefinidos = array(
 $user = isset($_POST['user']) ? trim(strip_tags($_POST['user'])) : '';
 $pass = isset($_POST['password']) ? trim(strip_tags($_POST['password'])) : '';
 
+if($user == ''){
+    $mensaje = 'No deje el campo usuario vacio';
+}
+if($pass == ''){
+    $mensaje = 'No deje el campo contraseña vacio';
+}
 
 ?>
