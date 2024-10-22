e916642239fdd0a7f046518517a0bece6fe6f3d6
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2024-10-22 20:03:11 -0300

    $mensaje = "";

--- PARCIALES/PARCIAL_3/index.php
+++ PARCIALES/PARCIAL_3/index.php
@@ -72,7 +72,7 @@
                     $mensaje = "3333";
                     break;
                 default:
-                    $mensaje = "Existe un error.";
+                    $mensaje = "";
             }
             if($mensaje !== ''){
                 echo sprintf('<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><span class="sr-only">Error:</span> %s </div>', $mensaje);
