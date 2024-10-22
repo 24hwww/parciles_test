89db83bbc399f2c78c18e673d9c0a625ac1018aa
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2024-10-22 19:49:41 -0300

    login.php

--- PARCIALES/PARCIAL_3/login.php
+++ PARCIALES/PARCIAL_3/login.php
@@ -1,1 +1,11 @@
-<?php session_start(); ?>
+<?php 
+session_start(); 
+$datos_predefinidos = array(
+'user' => 'demo1',
+'pass' => 'demo1'
+);
+$user = isset($_POST['user']) ? trim(strip_tags($_POST['user'])) : '';
+$pass = isset($_POST['password']) ? trim(strip_tags($_POST['password'])) : '';
+
+
+?>
