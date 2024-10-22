b500e994a68c7add7edf591e220f08f2498ff64d
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2024-10-22 19:39:44 -0300

    act1

--- PARCIALES/PARCIAL_3/index.php
+++ PARCIALES/PARCIAL_3/index.php
@@ -13,11 +13,8 @@
     <title>PARCIAL_3</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -25,15 +22,74 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-
   <body>
 
- 
+    <header>
+    <div class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">+</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.php">PARCIAL_3</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="./index.php">Inicio</a></li>
+            <li><a href="?tareas">Nueva Tarea</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Inicio de Sesión</h1>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8 col-sm-12 col-xs-12">
+            
+        <div class="panel panel-default">
+        <div class="panel-heading">
+        <h3 class="panel-title">Formulario de acceso</h3>
+        </div>
+        <div class="panel-body">
+            <form method="POST" action="login.php">
+
+                <div class="form-group">
+                    <label for="user">Usuario</label>
+                    <input type="text" id="user" class="form-control" autocomplete="off" required/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Contraseña</label>
+                    <input type="password" id="password" class="form-control" autocomplete="off" required/>
+                </div>
+
+                <button class="btn btn-success" type="submit">Entrar</button>
+
+            </form>
+        </div>
+        </div>        
+
+        </div>
+      </div>
+
+    </div>
+    </header>
+
+    <main>
+
+    </main>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
   </body>
 </html>
