d740870681ad78d9638c08ab0557df129958a9f0
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2024-10-22 21:14:48 -0300

    nueva-tarea

--- PARCIALES/PARCIAL_3/nueva-tarea.php
+++ PARCIALES/PARCIAL_3/nueva-tarea.php
@@ -6,7 +6,7 @@ $sesion_cookie = isset($_COOKIE['session']) ? $_COOKIE['session'] : [];
 $usuario_inicio_sesion = isset($sesion_cookie['user']) ? true : false;
 
 if($usuario_inicio_sesion !== true){
-    header('Location: ./index.php?error=1');
+    header('Location: ./index.php?pagina=nueva-tarea&error=1');
     exit;
 }
 
@@ -14,16 +14,20 @@ $tarea = isset($_POST['tarea']) ? trim(strip_tags($_POST['tarea'])) : '';
 $fecha_limite = isset($_POST['fecha_limite']) ? trim(strip_tags($_POST['fecha_limite'])) : '';
 
 if($tarea == '' || $fecha_limite == ''){
-    header('Location: ./index.php?error=2');
+    header('Location: ./index.php?pagina=nueva-tarea&error=2');
     exit;
 }
 
 list($day, $month, $year, $hour, $minute) = split('[/ :]', $fecha_limite); 
 $timestamp = mktime($hour, $minute, 0, $month, $day, $year);
-$fecha_limite = date("r", $timestamp);
+$fecha_limite_timestamp = date("r", $timestamp);
 $fecha_actual = date('Y-m-d');
+$fecha_actual_timestamp = strtotime("now");
 
-
+if($fecha_limite_timestamp < $fecha_actual_timestamp){
+    header('Location: ./index.php?pagina=nueva-tarea&error=3');
+    exit;
+}
 
 
 
